03fcc8e254edebe0ece3ce8c48189a970de47572
Victor Chang <victor@monkeytaps.app>
2026-02-28 16:31:28 +0100

    initial commit

---
+++ OfflineNews/OfflineNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineNewsApp.swift
+//  OfflineNews
+//
+//  Created by Victor Chang on 27/2/26.
+//
+
+import SwiftUI
+
+@main
+struct OfflineNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OfflineNewsTests/OfflineNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineNewsTests.swift
+//  OfflineNewsTests
+//
+//  Created by Victor Chang on 27/2/26.
+//
+
+import Testing
+@testable import OfflineNews
+
+struct OfflineNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
